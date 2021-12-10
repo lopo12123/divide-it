8fe2043de2f94cca4514083c1d805dec614a2025
chenmh <chenmh@sucsoft.com>
2021-12-10 13:43:05 +0800

    refactor: change the scope from 'split-it' to 'divide-it'

--- index.d.ts
+++ index.d.ts
@@ -1,44 +1,49 @@
-declare module 'split-it' {
-    namespace SplitIt {
-        interface SI {
+declare module 'divide-it' {
+    namespace DivideIt {
+        interface DI {
             /**
-             * @description Split the string into groups of n
+             * @public
+             * @description Divide the string into groups of n
              * @param {string} str origin string
              * @param {number} n number of characters in every group
              * @return {string[]}
              * @example
-             * SI.splitByN("AaBbCcDd", 2);  // output: ["Aa", "Bb", "Cc", "Dd"]
-             * SI.splitByN("AaBbCcDdE", 2);  // output: ["Aa", "Bb", "Cc", "Dd", "E"]
+             * DI.divideByN("AaBbCcDd", 2);  // output: ["Aa", "Bb", "Cc", "Dd"]
+             * DI.divideByN("AaBbCcDdE", 2);  // output: ["Aa", "Bb", "Cc", "Dd", "E"]
              */
-            splitByN(str: string, n: number): string[]
+            divideByN(str: string, n: number): string[]
 
             /**
-             * @description Split the string into groups of [...ns]
+             * @public
+             * @description divide the string into groups of [...ns]
              * @param {string} str origin string
              * @param {number[]} ns number of characters in groups(in order)
              * @param {number} rest how to divide the rest of the string
              * @return {string[]}
              * @example
-             * SI.splitByNs("ABbCcc", [1,2,3]);  // output: ["A", "Bb", "Ccc"]
-             * SI.splitByNs("ABbCccDd", [1,2,3,4]);  // output: ["A", "Bb", "Ccc", "Dd"]
-             * SI.splitByNs("ABbCccDddd", [1,2,3]);  // output: ["A", "Bb", "Ccc", "D", "d", "d", "d"]
-             * SI.splitByNs("ABbCccDddd", [1,2,3], 2);  // output: ["A", "Bb", "Ccc", "Dd", "dd"]
+             * DI.divideByNs("ABbCcc", [1,2,3]);  // output: ["A", "Bb", "Ccc"]
+             * DI.divideByNs("ABbCccDd", [1,2,3,4]);  // output: ["A", "Bb", "Ccc", "Dd"]
+             * DI.divideByNs("ABbCccDddd", [1,2,3]);  // output: ["A", "Bb", "Ccc", "D", "d", "d", "d"]
+             * DI.divideByNs("ABbCccDddd", [1,2,3], 2);  // output: ["A", "Bb", "Ccc", "Dd", "dd"]
              */
-            splitByNs(str: string, ns: number[], rest: number): string[]
+            divideByNs(str: string, ns: number[], rest: number): string[]
+
 
             /**
-             * @description Split letters and numbers
+             * @public
+             * @description Divide letters and numbers
              * @param {string} str origin string
-             * @param {('N'|'L'|'O')[]} filter N: `number`, L: `letter`, O: `other` <br/>
-             * declare which group(s) need to be filtered out in the split process
+             * @param {Array<'N'|'L'|'O'>} filter N: `number`, L: `letter`, O: `other` <br/>
+             * declare which group(s) need to be filtered out in the divide process
+             * @return {string[]}
              * @example
              * const str = '1jf啊j14+-啊15)4啊'
-             * SI.splitByLetterAndNumber(str)  // output: ['1', 'jf', '啊', 'j', '14', '+-啊', '15', ')', '4', '啊']
-             * SI.splitByLetterAndNumber(str, ['O'])  // output: ['1', 'jf', 'j', '14', '15', '4']
+             * DI.divideByType(str)  // output: ['1', 'jf', '啊', 'j', '14', '+-啊', '15', ')', '4', '啊']
+             * DI.divideByType(str, ['O'])  // output: ['1', 'jf', 'j', '14', '15', '4']
              */
-            splitByType(str: string, filter: ('N'|'L'|'O')[] = []): string[]
+            divideByType(str: string, filter: ('N'|'L'|'O')[] = []): string[]
         }
     }
-    const SI: SplitIt.SI;
-    export = SI;
+    const DI: DivideIt.DI;
+    export = DI;
 }
